658a94cd2c3dbec17c6698a9f2fec3055d9f5ccb
Francisco Sicardi <francisco.sicardi@despegar.com>
2020-03-31 17:08:48 -0300

    remove create-react-app default content and change favicon

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders paragraph', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const paragraph = getByText(/Nothing here/i);
+  expect(paragraph).toBeInTheDocument();
 });
