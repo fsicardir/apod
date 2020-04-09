cde63fdbe3d88a6864c02d683de4d123320d7a8f
Francisco Sicardi <francisco.sicardi@despegar.com>
2020-04-09 15:47:49 -0300

    Updates

---
+++ precache-manifest.96cdc7c37f410be673536d3aee27ac77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d0a9e18846dd8aa4d706a27af6f489",
+    "url": "/apod/index.html"
+  },
+  {
+    "revision": "5d7705cb01755d11289b",
+    "url": "/apod/static/css/main.685a530c.chunk.css"
+  },
+  {
+    "revision": "b1069f87c1fc860bacad",
+    "url": "/apod/static/js/2.352e9a8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apod/static/js/2.352e9a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7705cb01755d11289b",
+    "url": "/apod/static/js/main.fc0cfc3d.chunk.js"
+  },
+  {
+    "revision": "a9e8530d21c4b952ed77",
+    "url": "/apod/static/js/runtime-main.e0ac8f62.js"
+  }
+]);
