efa496565bf0585417d2f93ffa88ed0077997ab4
fsicardir <fsicardir@users.noreply.github.com>
2020-04-10 17:47:02 +0000

    deploy: e1a68a1f8cc45c7d4c361592367db10cf9f6e320

--- precache-manifest.10757bad01ea7d23eb7ede11435aef32.js
+++ precache-manifest.10757bad01ea7d23eb7ede11435aef32.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79abab4706c9ba6e32eae61f66795553",
+    "revision": "c5f5965751a270cd58a7507e230e87e0",
     "url": "/index.html"
   },
   {
-    "revision": "6bf67db7369ab37b0eff",
+    "revision": "af716e8dd4afd3f402d9",
     "url": "/static/css/main.685a530c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.352e9a8b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6bf67db7369ab37b0eff",
-    "url": "/static/js/main.b0ff77e0.chunk.js"
+    "revision": "af716e8dd4afd3f402d9",
+    "url": "/static/js/main.fcc7334d.chunk.js"
   },
   {
     "revision": "d03971578debc611e9e1",
